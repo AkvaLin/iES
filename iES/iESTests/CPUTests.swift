35b3a952080ed03b8f0048b568914f41e186c6b1
Nikita Pivovarov <nikita.pivovarov2003@gmail.com>
2024-06-09 00:09:00 +0300

    [Emulator] test: separate lda tests

--- iES/iESTests/CPUTests.swift
+++ iES/iESTests/CPUTests.swift
@@ -12,9 +12,17 @@ final class CPUTests: XCTestCase {
 
     var cpu: MockedCPU!
     
-    func testLDA() {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
         cpu = MockedCPU()
-        
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        cpu = nil
+    }
+    
+    func testLDA() {
         let address: UInt16 = 0x0000
         let value: UInt8 = 0x05
         cpu.ram[Int(address % 0x0800)] = value
@@ -23,7 +31,10 @@ final class CPUTests: XCTestCase {
         XCTAssertEqual(value, cpu.a)
         XCTAssertFalse(cpu.z)
         XCTAssertFalse(cpu.n)
-        
+    }
+    
+    func testLDAZero() {
+        let address: UInt16 = 0x0000
         let secondValue: UInt8 = 0x00
         cpu.ram[Int(address % 0x0800)] = secondValue
         cpu.lda(address: address)
@@ -31,7 +42,10 @@ final class CPUTests: XCTestCase {
         XCTAssertEqual(secondValue, cpu.a)
         XCTAssertTrue(cpu.z)
         XCTAssertFalse(cpu.n)
-        
+    }
+    
+    func testLDANegative() {
+        let address: UInt16 = 0x0000
         let thirdValue: UInt8 = 0xFF
         cpu.ram[Int(address % 0x0800)] = thirdValue
         cpu.lda(address: address)
